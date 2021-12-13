55e70629354eb49fa1a996d9cda58887b5ea053e
Evan James <thevan777@gmail.com>
2021-12-13 16:50:29 -0500

    added auth user model to settings

---
+++ api/models/user.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('User must have email')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        
+        return user 
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_lemgth=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
