59fa3e0a76523618690cba80e604d7eef97fd2f0
Evan James <thevan777@gmail.com>
2021-12-13 16:58:11 -0500

    ran user migration

--- api/models/user.py
+++ api/models/user.py
@@ -17,12 +17,12 @@ class UserManager(BaseUserManager):
         user.is_superuser = True
         user.is_staff = True
         user.save()
-        
+
         return user 
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_lemgth=255, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
